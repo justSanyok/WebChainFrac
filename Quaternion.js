07548e2dce7142a8ee3193bf8215a786129ec177
AZbestFlax <azb@ukr.net>
2014-06-12 18:20:19 +0300

    Q ver 1.0

--- Quaternion.js
+++ Quaternion.js
@@ -2,7 +2,7 @@
 //============================== Q U A T E R N I O N ===========================================//
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-function Quaternion(Re, I, J, K) {
+function Quaternion(Re=0, I=0, J=0, K=0) {
 	this.re=Re;
 	this.i=I;
 	this.j=J;
@@ -28,20 +28,46 @@ Quaternion.prototype.addTo = function(term)  {
 	this.k+=term.k;
 }
 
-Quaternion.prototype.add = function(term1,term2) {
-	return Quaternion(term1.re+term2.re, term1.i+term2.i, term1.j+term2.j, term1.k+term2.k);
+Quaternion.prototype.add = function(term2,result) {
+	result.re = this.re+term2.re;
+	result.i = this.re+term2.i;
+	result.j = this.re+term2.j;
+	result.k = this.re+term2.k;
 }
 
-Quaternion.prototype.mult = function(m1,m2) {
-	var re=m1.re*m2.re-m1.i*m2.i-m1.j*m2.j-m1.k*m2.k;
-	var im=m1.re*m2.i+m1.i*m2.re+m1.j*m2.k-m1.k*m2.j;
-	var jm=m1.re*m2.j-m1.i*m2.k+m1.j*m2.re+m1.k*m2.i;
-	var km=m1.re*m2.k+m1.i*m2.j-m1.j*m2.i+m1.k*m2.re;
-	return Quaternion(re,im,jm,km);
+Quaternion.prototype.mult = function(m2,mr) {
+	mr.re=this.re*m2.re-this.i*m2.i-this.j*m2.j-this.k*m2.k;
+	mr.i=this.re*m2.i+this.i*m2.re+this.j*m2.k-this.k*m2.j;
+	mr.j=this.re*m2.j-this.i*m2.k+this.j*m2.re+this.k*m2.i;
+	mr.k=this.re*m2.k+this.i*m2.j-this.j*m2.i+this.k*m2.re;
+	//return Quaternion(re,im,jm,km);
+}
+
+Quaternion.prototype.norma = function() {
+	return this.re*this.re+this.i*this.i+this.j*this.j+this.k*this.k;
+}
+
+Quaternion.prototype.converse = function() {
+	var n = this.norma();
+	this.re/=n;
+	this.i/=-n;
+	this.j/=-n;
+	this.k/=-n;
+}
+
+Quaternion.prototype.division = function(denominator,left,right) {
+	denominator.converse();
+	this.mult(denominator,left);
+	denominator.mult(this,right);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 function mainWorkFunction2() {
-	var q = new Quaternion(3,0,2,1); 
-	alert(q.toString());
+	var q1 = new Quaternion(1,1,1,0); 
+	var q2 = new Quaternion(1,1,0,0); 
+	var l = new Quaternion();
+	var r = new Quaternion();
+	q1.division(q2,l,r);
+	alert(l.toString());
+	alert(r.toString());
 }
