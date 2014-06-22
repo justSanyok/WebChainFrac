bb2a42c5f423dce5f0216565328f51ee59307c03
AZbestFlax <azb@ukr.net>
2014-06-22 13:32:51 +0300

    some improves

--- Chain.js
+++ Chain.js
@@ -1,17 +1,21 @@
-﻿// Допоміжні функції
+﻿/* Допоміжні функції */
 function gcd(a,b) {
 	return (b)?gcd(b,a%b):a;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////========== F R A C ==========///////////////////////////////////////
+/*//////////////////////////////////////========== F R A C ==========/////////////////////////////////////*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
  k*(n+sqrt(f))/d
 */
 
 //конструктор
-function Frac(radical=0, term=0, denominator=1, coefficient=1) {
+function Frac(parameters) {
+    var radical = parameters.radical;
+    var term = parameters.term;
+    var denominator = parameters.denominator;
+    var coefficient = parameters.coefficient;
     this.n = term;      	// доданок
     this.f = radical; 		// підкореневий вираз
 	this.d = denominator; 	// знаменник
@@ -21,19 +25,19 @@ function Frac(radical=0, term=0, denominator=1, coefficient=1) {
 //отримання окремих елементів дробу
 Frac.prototype.getN = function() {
 	return this.n;
-}
+};
 
 Frac.prototype.getF = function() {
 	return this.f;
-}
+};
 
 Frac.prototype.getD = function() {
 	return this.d;
-}
+};
 
 Frac.prototype.getK = function() {
 	return this.k;
-}
+};
 
 Frac.prototype.toString = function() {
     return this.k + "*(" + this.n + "+V" + this.f + ")/" + this.d;
@@ -51,54 +55,54 @@ Frac.prototype.equals = function(that) {
            (this.n == that.n)&&
            (this.d == that.d)&&
            (this.k == that.k);
-}
+};
 
 // додавання цілого числа до дробу
 Frac.prototype.addValueToFrac = function(value) {
 	var _f = this.f*this.k*this.k;
     var _n = this.k*this.n + value*this.d;
     if (this.k<0) _n*=-1;
-    var kk;
-    (this.k<0)?kk=-1:kk=1;
-    var tmp = new Frac(_f,_n,this.d,kk);
+    var kk=1;
+    if(this.k<0) kk=-1;
+    var tmp = new Frac({radical: _f, term: _n, denominator: this.d, coefficient: kk});
     tmp.simplify();
     return tmp;
-}
+};
 
-//віднімання цілого від дробу
+/* віднімання цілого від дробу */
 Frac.prototype.minusValueToFrac = function(value) {
 	var _f = this.f*this.k*this.k;
     var _n = this.k*this.n - value*this.d;
     if (this.k<0) _n*=-1;
-    var kk;
+    var kk=1;
     (this.k<0)?kk=-1:kk=1;
-    var tmp = new Frac(_f,_n,this.d,kk);
+    var tmp = new Frac({radical: _f, term: _n, denominator: this.d, coefficient: kk});
     tmp.simplify();
     return tmp;
-}
+};
 
-//визначення максимального цілого z<frac
+/* визначення максимального цілого z<frac */
 Frac.prototype.toInteger = function() {
 	return Math.floor(this.k*(Math.sqrt(this.f)+this.n)/this.d);
-} 
+};
  
 Frac.prototype.isFullSquare =function() {
 	return Math.abs(Math.floor(Math.sqrt(this.f))-Math.sqrt(this.f))<1e-8;
-} 
+};
 
 Frac.prototype.simplify =function() {
 	var u;
 	var _n;
 	if (this.d<0) {this.d*=-1; this.k*=-1;}
-//========================================== if simple frac	
-        if (this.f==0) {
+/*========================================== if simple frac	*/
+    if (this.f==0) {
             _n = this.n*this.k;
-            u = gcd(Math.abs(_n),Math.abs(d));
+            u = gcd(Math.abs(_n),Math.abs(this.d));
             this.n=_n/u;
             this.d/=u;
             this.k=1;
         } else {
-//========================================== radiacal is full square
+/*========================================== radiacal is full square*/
             if (this.isFullSquare()) {
                 _n=this.k*(Math.floor(Math.sqrt(this.f))+this.n);
                 this.f=0;
@@ -107,7 +111,7 @@ Frac.prototype.simplify =function() {
                 this.d/=u;
                 this.k=1;
             }
-//========================================== extract from radical
+/*========================================== extract from radical*/
             u = Math.sqrt(this.f);
             var result = 1;
 			var i = 2;
@@ -116,57 +120,56 @@ Frac.prototype.simplify =function() {
                     result *= i;
                     this.f/=(i*i);
                 }
-            };
-//========================================= extract from brackets
+            }
+/*========================================= extract from brackets*/
             u = gcd(result, Math.abs(this.n));
             this.k*=u;
             result/=u;
             this.n/=u;
             this.f*=(result*result);
-//========================================= reduce
+/*========================================= reduce*/
             u = gcd(Math.abs(this.k),Math.abs(this.d));
             this.k/=u;
             this.d/=u;
         }
-}
+};
 
 Frac.prototype.converse = function() {
 	var new_d = this.k*(this.f-this.n*this.n);
-	var tmp = new Frac(this.f,-this.n,new_d,this.d);
+	var tmp = new Frac({radical: this.f, term: -this.n, denominator: new_d, coefficient: this.d});
 	tmp.simplify();
 	return tmp;
+};
+
+/*  попередньо визначені дробові числа */
+Frac.ZERO = new Frac({radical: 0, term: 0, denominator: 1, coefficient: 0});
+Frac.ONE = new Frac({radical: 0, term: 1, denominator: 1, coefficient: 1});
+
+
+/*===========================================================================================================*/
+/*================================================ C H A I N ================================================*/
+/*===========================================================================================================*/
+function Chain(parameters) {
+    var frac = parameters.frac;
+    var arrV = parameters.arrayVariables;
+    var arrW = parameters.arrayReminders;
+    var startPeriod = parameters.startPeriod;
+    var endPeriod = parameters.endPeriod;
+    var p = parameters.isPeriodical;
+    this.f = frac; 	     	/* дріб                                     */
+    this.v = arrV;	        /* масив чисел                              */
+    this.w = arrW;	        /* масив залишків                           */
+    this.p = p; 		    /* періодичність (boolean)                  */
+	this.sp = startPeriod;	/* номер елемента початку періоду в масиві  */
+    this.ep = endPeriod;	/* номер елемента кінця періоду в масиві    */
 }
 
-// попередньо визначені дробові числа 
-Frac.ZERO = new Frac(0,0,1,0);
-Frac.ONE = new Frac(0,1,1,1);
-
-//=============================================================================================================
-//================================================ P O L Y N O M ==============================================
-//=============================================================================================================
-
-
-
-//=============================================================================================================
-//================================================ C H A I N ==================================================
-//=============================================================================================================
-function Chain(frac) {
-    this.f = frac; 	     	// дріб
-    this.v = new Array();	// масив чисел
-    this.w = new Array();	// масив фігні
-    this.p = false; 		// періодичність (boolean)
-	//this.sp = startPeriod;	// номер елемента початку періоду в масиві 
-	//this.ep = endPeriod;	// номер елемента кінця періоду в масиві 
-}
-
-//*   
-Chain.prototype.find = function(frac) {
-       
-}
-// */
+Chain.prototype.toString = function() {
+    return this.f.toString();
+};
 
 function mainWorkFunction() {
-	var frac = new Frac(3,2,2); 
-	var chain = new Chain(frac);
-	alert(chain.f.toString());
+    var fr = new Frac({radical: 3, term: 2, denominator: 2, coefficient: 1});
+	var chain = new Chain({frac: fr});
+	alert(chain.toString());
 }
--- Quaternion.js
+++ Quaternion.js
@@ -1,8 +1,12 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////
 //============================== Q U A T E R N I O N ===========================================//
-//////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////*/
 
-function Quaternion(Re=0, I=0, J=0, K=0) {
+function Quaternion(parameters) {
+    var Re = parameters.Re;
+    var I = parameters.I;
+    var J = parameters.J;
+    var K = parameters.K;
 	this.re=Re;
 	this.i=I;
 	this.j=J;
@@ -31,39 +35,39 @@ Quaternion.prototype.toString = function() {
         if (t==1) {s+="k";} else {s+= t + "k";}
     }	
 	return s; 
-}
+};
 
 Quaternion.prototype.addTo = function(term)  {
 	this.re+=term.re;
 	this.i+=term.i;
 	this.j+=term.j;
 	this.k+=term.k;
-}
+};
 
 Quaternion.prototype.minusFrom = function(term)  {
 	this.re-=term.re;
 	this.i-=term.i;
 	this.j-=term.j;
 	this.k-=term.k;
-}
+};
 
 Quaternion.prototype.add = function(term2,result) {
 	result.re = this.re+term2.re;
 	result.i = this.re+term2.i;
 	result.j = this.re+term2.j;
 	result.k = this.re+term2.k;
-}
+};
 
 Quaternion.prototype.mult = function(m2,mr) {
 	mr.re=this.re*m2.re-this.i*m2.i-this.j*m2.j-this.k*m2.k;
 	mr.i=this.re*m2.i+this.i*m2.re+this.j*m2.k-this.k*m2.j;
 	mr.j=this.re*m2.j-this.i*m2.k+this.j*m2.re+this.k*m2.i;
 	mr.k=this.re*m2.k+this.i*m2.j-this.j*m2.i+this.k*m2.re;
-}
+};
 
 Quaternion.prototype.norma = function() {
 	return this.re*this.re+this.i*this.i+this.j*this.j+this.k*this.k;
-}
+};
 
 Quaternion.prototype.converse = function() {
 	var n = this.norma();
@@ -71,17 +75,16 @@ Quaternion.prototype.converse = function() {
 	this.i/=-n;
 	this.j/=-n;
 	this.k/=-n;
-}
+};
 
 Quaternion.prototype.division = function(denominator,left,right) {
 	denominator.converse();
 	this.mult(denominator,left);
 	denominator.mult(this,right);
-}
+};
 
-//////////////////////////////////////////////////////////////////////////////////////////////////
+/*//////////////////////////////////////////////////////////////////////////////////////////////*/
 function mainWorkFunction2() {
-	var q1 = new Quaternion(0,-1,0,9); 
+	var q1 = new Quaternion({Re: 0, I: -1, J: 0, K: 9});
 	alert(q1.toString());
-;
 }
